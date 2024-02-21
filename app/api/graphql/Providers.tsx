bef4f5d65b0c4278e679d43cb18c7ced27b2097c
Dini <dini@Dinis-MacBook-Air.local>
2024-02-22 06:13:33 +0800

    added graphql

--- app/api/graphql/Providers.tsx
+++ app/api/graphql/Providers.tsx
@@ -1,14 +1,30 @@
 "use client";
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { ReactNode } from "react";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseApiKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const httpLink = createHttpLink({
+  uri: supabaseUrl
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      Authorization: supabaseApiKey ? `Bearer ${supabaseApiKey}` : ""
+    }
+  };
+});
 
 export const Providers = ({ children }: { children: ReactNode }) => {
+  console.log(authLink.concat(httpLink));
+
   const client = new ApolloClient({
-    uri: supabaseUrl,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
   });
 
