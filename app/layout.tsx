76fdbaa307cf74bcd44cd1fc626a6dc79523ba80
Dini <dini@Dinis-MacBook-Air.local>
2024-02-23 00:43:05 +0800

    modified vercel url

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import Layout from "@/components/Layout";
 
-const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
+const defaultUrl = process.env.VERCEL_URL ? process.env.VERCEL_URL : "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
