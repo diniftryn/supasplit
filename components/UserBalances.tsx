5e5e5b0834b8168019f7da9ec64236b4f7e8dadc
Dini <dini@Dinis-MacBook-Air.local>
2024-02-24 04:08:14 +0800

    added user balances

--- components/UserBalances.tsx
+++ components/UserBalances.tsx
@@ -1,64 +1,59 @@
 "use client";
 
 import { createClient } from "@/utils/supabase/client";
-import { useCallback, useState } from "react";
 
-export default function UserBalances({ groupId }: { groupId: any }) {
+export default async function UserBalances({ groupId }: { groupId: any }) {
   const supabase = createClient();
   let owedAmounts = {};
-  const [group, setGroup] = useState({});
 
-  useCallback(() => {
-    async function getData() {
-      const { data: group, error: errorGroup } = await supabase.from("groups").select().eq("id", groupId);
-      const { data: expenses, error: errorExpenses } = await supabase.from("expenses").select();
-      const { data: participants, error: errorParticipants } = await supabase.from("participants").select();
-      const { data: payments, error: errorPayments } = await supabase.from("payments").select();
+  const { data: group, error: errorGroup } = await supabase.from("groups").select().eq("id", groupId);
+  const { data: expenses, error: errorExpenses } = await supabase.from("expenses").select();
+  const { data: participants, error: errorParticipants } = await supabase.from("participants").select();
+  const { data: payments, error: errorPayments } = await supabase.from("payments").select();
 
-      if (errorGroup || errorExpenses || errorParticipants || errorPayments) return <p>Unable to fetch User Balances.</p>;
-      if (!errorExpenses && !errorParticipants && !errorPayments) {
-        owedAmounts = calculateOwedAmounts(expenses, participants, payments);
-      }
-      if (group) setGroup(group);
-    }
+  if (errorGroup || errorExpenses || errorParticipants || errorPayments) return <p>Unable to fetch User Balances.</p>;
+  if (!errorExpenses && !errorParticipants && !errorPayments) {
+    owedAmounts = calculateOwedAmounts(expenses, participants, payments, group);
+  }
 
-    getData();
-  }, []);
+  const { data: users, error: errorUsers } = await supabase.from("users").select().in("id", group[0].userIds);
+  console.log(JSON.stringify(users));
 
   return (
     <div>
       {group &&
-        group.users.map((user: any) => (
+        group[0].userIds.map((userId: any) => (
           <p>
-            {user} {owedAmounts[user] < 0 ? " is owed " : " owes "} {owedAmounts[user]}
+            {userId}
+            {owedAmounts[userId] < 0 ? " is owed " : " owes "} {owedAmounts[userId]}
           </p>
         ))}
     </div>
   );
 }
 
-function calculateOwedAmounts(expenses: any, participants: any, payments: any) {
+function calculateOwedAmounts(expenses: any, participants: any, payments: any, group: any) {
   const userBalances: any = {};
 
   group &&
-    group[0].users.forEach((user: any) => {
+    group[0].userIds.forEach((user: any) => {
       userBalances[user] = 0;
       console.log("step 1 group memebers initialize userBalance: " + userBalances);
     });
 
-  participants.forEach((participant: { user_id: string | number; share_amount: number }) => {
-    userBalances[participant.user_id] += participant.share_amount;
+  participants.forEach((participant: { userId: string | number; shareAmount: number }) => {
+    userBalances[participant.userId] += participant.shareAmount;
     console.log("step 2 participants: " + userBalances);
   });
 
-  expenses.forEach((expense: { payer_id: any; amount: number }) => {
-    userBalances[expense.payer_id] -= expense.amount;
+  expenses.forEach((expense: { payerId: any; amount: number }) => {
+    userBalances[expense.payerId] -= expense.amount;
     console.log("step 3 expenses: " + userBalances);
   });
 
-  payments.forEach((payment: { payer_id: string | number; amount: number; payee_id: string | number }) => {
-    userBalances[payment.payer_id] -= payment.amount;
-    userBalances[payment.payee_id] += payment.amount;
+  payments.forEach((payment: { payerId: string | number; amount: number; payeeId: string | number }) => {
+    userBalances[payment.payerId] -= payment.amount;
+    userBalances[payment.payeeId] += payment.amount;
     console.log("step 4 payments: " + userBalances);
   });
 
