398688e26a3802eee959a1038978e8fc8712f4a9
Dini <dini@Dinis-MacBook-Air.local>
2024-02-23 16:22:00 +0800

    modify group form

--- components/GroupForm.tsx
+++ components/GroupForm.tsx
@@ -85,9 +85,11 @@ export default function GroupForm() {
           </div>
         </div>
 
-        <button type="submit" className="py-2 px-5 bg-lime-300 dark:bg-lime-700 rounded-3xl flex justify-center">
-          Create Group
-        </button>
+        <div className="flex justify-center pt-5">
+          <button type="submit" className="py-2 px-5 bg-lime-300 dark:bg-lime-700 rounded-3xl">
+            Create Group
+          </button>
+        </div>
       </form>
     </div>
   );
