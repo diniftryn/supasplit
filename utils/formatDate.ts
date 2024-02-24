422f9d31f9fc5705395661245259fb99ff1db2f5
Dini <dini@Dinis-MacBook-Air.local>
2024-02-25 01:34:11 +0800

    fixed formatdate typescript errors

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,14 +1,24 @@
 export default function formatDateTime(datetime: any) {
   const date = new Date(datetime);
 
+  type DateFormatOptions = {
+    day: "2-digit";
+    month: "short";
+  };
+
   // Format the date as "dd-mmm"
-  const dateFormatOptions = { day: "2-digit", month: "short" };
+  const dateFormatOptions: DateFormatOptions = { day: "2-digit", month: "short" };
   const formattedDate = date.toLocaleDateString("en-US", dateFormatOptions);
   const [month, day] = formattedDate.split(" ");
   const formattedDateReverse = `${day} ${month}`;
 
+  type TimeFormatOptions = {
+    hour: "2-digit";
+    minute: "2-digit";
+  };
+
   // Format the time
-  const timeFormatOptions = { hour: "2-digit", minute: "2-digit" };
+  const timeFormatOptions: TimeFormatOptions = { hour: "2-digit", minute: "2-digit" };
   const formattedTime = date.toLocaleTimeString("en-US", timeFormatOptions);
 
   // Combine the formatted date and time
